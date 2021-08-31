2674c29adfaac16acad8be2ac8caec716f5c0131
moshtab <mdmohsin41439@gmail.com>
2021-08-31 21:58:32 +0530

    Updated salary of Terisa in Employee payroll

--- src/main/java/basic/day34JDBC_EmployeePayrollSerevice/EmployeePayrollServiceJDBC.java
+++ src/main/java/basic/day34JDBC_EmployeePayrollSerevice/EmployeePayrollServiceJDBC.java
@@ -2,13 +2,43 @@ package basic.day34JDBC_EmployeePayrollSerevice;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
 public class EmployeePayrollServiceJDBC {
 	public static void main(String[] args) {
+		connectionEshtablished();
+		readEmployeePayroll();
+		updateEmployeePayroll();
+
+	}
+
+	private static Connection getSqlConnection() {
+		Connection conn = null;
+		String dbHostUrl = "jdbc:mysql://localhost:3306/payroll_service";
+		String userName = "root";
+		String password = "Md78678678.";
+		try {
+			conn = DriverManager.getConnection(dbHostUrl, userName, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conn;
+
+	}
+
+	private static void connectionEshtablished() {
+		Connection conn = getSqlConnection();
+		if (conn != null) {
+			System.out.println("Connection is eshtablished");
+		}
+	}
+
+	private static void readEmployeePayroll() {
+		System.out.println("Displaying all data of employee_payroll table");
 		Connection conn = getSqlConnection();
 
 		try {
@@ -44,19 +74,32 @@ public class EmployeePayrollServiceJDBC {
 
 	}
 
-	private static Connection getSqlConnection() {
-		Connection conn = null;
-		String dbHostUrl = "jdbc:mysql://localhost:3306/payroll_service";
-		String userName = "root";
-		String password = "Md78678678.";
-		try {
-			conn = DriverManager.getConnection(dbHostUrl, userName, password);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	private static void updateEmployeePayroll() {
+		System.out.println("Updating salary of Terisa ");
+		Connection conn = getSqlConnection();
+		if (conn != null) {
+			String updateEmpPayroll = "UPDATE employee_payroll SET salary = ? WHERE name ='Terisa'";
+			try {
+				PreparedStatement preparedStatement = conn.prepareStatement(updateEmpPayroll);
+				preparedStatement.setInt(1, 300000);
+				int rowUpdated = preparedStatement.executeUpdate();
+				if (rowUpdated > 0) {
+					System.out.println("Data is Updated");
+				}
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+			} finally {
+				if (conn != null) {
+					try {
+						conn.close();
+					} catch (SQLException sqlException) {
+						System.out.println(sqlException.getMessage());
+
+					}
+				}
+			}
 		}
-		return conn;
 
 	}
-
 }

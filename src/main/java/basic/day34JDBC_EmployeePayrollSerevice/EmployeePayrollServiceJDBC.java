1c8ce962733389286fde4f30e72ee78c218a8752
moshtab <mdmohsin41439@gmail.com>
2021-08-31 19:02:07 +0530

    Readed all employeedata from table

--- src/main/java/basic/day34JDBC_EmployeePayrollSerevice/EmployeePayrollServiceJDBC.java
+++ src/main/java/basic/day34JDBC_EmployeePayrollSerevice/EmployeePayrollServiceJDBC.java
@@ -1,7 +1,39 @@
 package basic.day34JDBC_EmployeePayrollSerevice;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class EmployeePayrollServiceJDBC {
 	public static void main(String[] args) {
-		System.out.println("Welcome to JDBC Employee payroll Service");
+		Connection conn = getSqlConnection();
+		if (conn != null) {
+			System.out.println("Connection is eshtablished");
+		}
+
 	}
+
+	private static Connection getSqlConnection() {
+		Connection conn = null;
+		String dbHostUrl = "jdbc:mysql://localhost:3306/payroll_service";
+		String userName = "root";
+		String password = "Md78678678.";
+		try {
+			conn = DriverManager.getConnection(dbHostUrl, userName, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException sqlException) {
+					System.out.println(sqlException.getMessage());
+
+				}
+			}
+		}
+		return conn;
+	}
+
 }

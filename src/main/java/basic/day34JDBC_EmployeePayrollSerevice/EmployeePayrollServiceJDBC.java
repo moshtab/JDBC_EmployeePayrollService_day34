66fd9d9045c06c2c61ed6813f3b4e50319b8a576
moshtab <mdmohsin41439@gmail.com>
2021-08-31 19:10:50 +0530

    Readed all data from employee_payroll table

--- src/main/java/basic/day34JDBC_EmployeePayrollSerevice/EmployeePayrollServiceJDBC.java
+++ src/main/java/basic/day34JDBC_EmployeePayrollSerevice/EmployeePayrollServiceJDBC.java
@@ -2,13 +2,44 @@ package basic.day34JDBC_EmployeePayrollSerevice;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class EmployeePayrollServiceJDBC {
 	public static void main(String[] args) {
 		Connection conn = getSqlConnection();
-		if (conn != null) {
-			System.out.println("Connection is eshtablished");
+
+		try {
+			if (conn != null) {
+				String readEmpPayroll = "SELECT * FROM employee_payroll";
+
+				Statement statement = conn.createStatement();
+				ResultSet resultSet = statement.executeQuery(readEmpPayroll);
+				while (resultSet.next()) {
+					Integer id = resultSet.getInt(1);
+					String name = resultSet.getString(2);
+					Integer salary = resultSet.getInt(3);
+					String date = resultSet.getString(4);
+					String gender = resultSet.getString(5);
+					String row = String.format(
+							"User record: \n Id: %d, \n Name: %s,\n Salary: %d, \n Date: %s,  \n Gender: %s \n", id,
+							name, salary, date, gender);
+					System.out.println(row);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException sqlException) {
+					System.out.println(sqlException.getMessage());
+
+				}
+			}
 		}
 
 	}
@@ -23,17 +54,9 @@ public class EmployeePayrollServiceJDBC {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} finally {
-			if (conn != null) {
-				try {
-					conn.close();
-				} catch (SQLException sqlException) {
-					System.out.println(sqlException.getMessage());
-
-				}
-			}
 		}
 		return conn;
+
 	}
 
 }

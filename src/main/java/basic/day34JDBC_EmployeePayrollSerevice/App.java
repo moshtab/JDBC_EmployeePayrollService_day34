ae799ce214e96dbfaa9ffd6ba8c7058a0c64da92
moshtab <mdmohsin41439@gmail.com>
2021-08-31 18:59:23 +0530

    added msg

---
+++ src/main/java/basic/day34JDBC_EmployeePayrollSerevice/App.java
@@ -0,0 +1,13 @@
+package basic.day34JDBC_EmployeePayrollSerevice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/basic/day34JDBC_EmployeePayrollSerevice/EmployeePayrollServiceJDBC.java
@@ -0,0 +1,7 @@
+package basic.day34JDBC_EmployeePayrollSerevice;
+
+public class EmployeePayrollServiceJDBC {
+	public static void main(String[] args) {
+		System.out.println("Welcome to JDBC Employee payroll Service");
+	}
+}
